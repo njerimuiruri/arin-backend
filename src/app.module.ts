67b932aa0e20321166bd9da1ad12b1e4f0766002
safravo <safravoltd@gmail.com>
2026-01-08 09:52:14 +0300

    code updates

--- src/app.module.ts
+++ src/app.module.ts
@@ -24,6 +24,7 @@ import { ResearchProjectsModule } from './research-projects/research-projects.mo
     UsersModule,
     AuthModule,
     ResearchProjectsModule,
+    
   ],
   controllers: [AppController],
   providers: [AppService],
